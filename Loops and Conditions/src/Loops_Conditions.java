3dfc8e7dc79309954fe8151bf38cf874cd388a13
Shrawan99 <rautshrawan30@gmail.com>
2016-03-02 16:21:05 +0545

    if else examples

---
+++ Loops and Conditions/src/Loops_Conditions.java
@@ -0,0 +1,24 @@
+
+public class Loops_Conditions {
+
+	public static void main(String[] args) {
+		
+		String pwd="Shrawan";
+		
+		if (pwd=="Shrawan")
+			
+		{
+		System.out.println("!!!Welcome Home Shrawan!!!");	
+		}
+		
+		else
+		{
+			System.out.println("You are Thief");	
+			System.out.println("You are Thief");
+			System.out.println("You are Thief");
+			System.out.println("You are Thief");
+		}
+
+	}
+
+}

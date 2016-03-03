ec6955a0ce75d4945b6ecd8023835a3421339d19
Shrawan99 <rautshrawan30@gmail.com>
2016-03-03 08:56:19 +0545

    Some Math Functions

--- Loops and Conditions/src/Loops_Conditions.java
+++ Loops and Conditions/src/Loops_Conditions.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 
 public class Loops_Conditions {
 
 	public static void main(String[] args) {
 		
-		String pwd="Shrawan";
+		System.out.println("!!!Welcome to Home Security System!!!\nPlease verify yourself\nEnter Your Password");	
+		Scanner sc=new Scanner(System.in);//Input from User
+		String password=sc.next();
+		String pwd1=password.trim();
+		String pwd="12345";
 		
-		if (pwd=="Shrawan")
-			
+		if (pwd1==pwd)	
 		{
 		System.out.println("!!!Welcome Home Shrawan!!!");	
 		}

a1db67cb7e1dfce894ce86eed7a5a39ee7a430aa
Shrawan99 <rautshrawan30@gmail.com>
2016-03-23 18:11:54 +0545

    Wednesday

---
+++ Swing_Examples/src/SwingCalculator.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+class SwingCalculator {
+   public static void main(String[] args) {
+        JFrame frame = new Calculator_Calc();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
